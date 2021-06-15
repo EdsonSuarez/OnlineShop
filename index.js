83c640060efa2e24dfde5796d31addc17d205e6a
EdsonSuarez <edsondz43@gmail.com>
2021-06-15 10:49:20 -0500

    add admin, cliente, auth, role, user

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const { dbConnection } = require("./db/db");
 const cors = require("cors");
 require("dotenv").config();
 
+const User = require("./routes/user");
+const Auth = require("./routes/auth");
+const Role = require("./routes/role");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/api/auth/", Auth);
+app.use("/api/user/", User);
+app.use("/api/role/", Role);
+
 
 app.listen(process.env.PORT, () => console.log("localhost:",process.env.PORT));
 
